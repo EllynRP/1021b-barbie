f770571ab38ff54d719f419bf06646cb792262d7
EllynRP <evelyn.pereira2@estudante.ifms.edu.br>
2023-09-05 08:28:01 -0400

    aula 05092023

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -1,27 +1,39 @@
 import './Header.css'
 function Header(){
+    let texto = "terenciane"
+    function mudaTexto(){
+        texto = "terenciane"
+    }
     return(
-        <header>
+        <>
+            <header>
+                <div>
+                    logo
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <input className='barrapesquisa' type='text'/>
+                        </li>
+                        <li>
+                            <a href="#">Página inicial</a>
+                        </li>
+                        <li>
+                            <a href="#">Sobre</a>
+                        </li>
+                        <li>
+                            <a href="#">Notícias</a>
+                        </li>
+                        <li>
+                            <a href="#">Contato</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
             <div>
-                logo
+                <p className='texto_digitado'>pesquisa: {texto}</p>
             </div>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#">Página inicial</a>
-                    </li>
-                    <li>
-                        <a href="#">Sobre</a>
-                    </li>
-                    <li>
-                        <a href="#">Notícias</a>
-                    </li>
-                    <li>
-                        <a href="#">Contato</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        </>
     )
 }
 export default Header
--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -14,6 +14,21 @@ export default function Main(){
                 sinopse= "O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons, chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam." 
                 imagem="/transformerBanner.jpg"
             />
+            <Filme
+                titulo="transformers" 
+                sinopse= "O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons, chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam." 
+                imagem="/transformerBanner.jpg"
+            />
+            <Filme
+                titulo="transformers" 
+                sinopse= "O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons, chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam." 
+                imagem="/transformerBanner.jpg"
+            />
+            <Filme
+                titulo="transformers" 
+                sinopse= "O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons, chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam." 
+                imagem="/transformerBanner.jpg"
+            />
         </main>
     )
 }
