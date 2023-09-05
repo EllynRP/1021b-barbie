ffd10805a3e1612face1a57ab240873ace5ac1ed
EllynRP <evelyn.pereira2@estudante.ifms.edu.br>
2023-09-05 07:34:18 -0400

    meu commit

---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import Logo from '../../assets/barbieLogo.png'
+function Footer(){
+    return(
+        <footer>
+            <img src={Logo} alt="" />
+        </footer>
+    )
+}
+export default Footer
